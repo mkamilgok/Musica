e97c3ceab278bac25117336d67d22e3d688fe0c1
mkamilgok <mkamilgok@gmail.com>
2020-08-10 22:03:05 +0300

    Add time range options

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -3,7 +3,14 @@ import "./SearchBar.css";
 
 function SearchBar(props){
 
+    const timeRanges = {
+        "Lately Listened (Last 4 weeks)" : "sort_term",
+        "Medium Time Range (Last 6 months)" : "medium_term",
+        "All Of My Spotify History" : "long_term"
+    };
+
     const[term, setTerm] = useState("");
+    const[timeRange, setTimeRange] = useState("medium_term");
 
     const search = () => {
         props.onSearch(term);
@@ -13,11 +20,35 @@ function SearchBar(props){
         setTerm(event.target.value);
     }
 
+    function getSearchByClass(searchByOption){
+        if(timeRange === searchByOption){
+            return "active";
+        }
+        return "";
+    }
+
     return(
-        <div className="SearchBar">
-            <input placeholder="Enter A Song, Album, or Artist" onChange={handleTermChange}/>
-            <button className="SearchButton" onClick={search}>SEARCH</button>
+        <div>
+            <div className="SearchBar-search-options">
+                <ul>
+                    {Object.keys(timeRanges).map(searchByOption => {
+                        let searchByOptionValue = timeRanges[searchByOption];
+                        return <li
+                                    onClick={() => setTimeRange(searchByOptionValue)}
+                                    className={getSearchByClass(searchByOptionValue)}
+                                    key={searchByOptionValue}
+                                >
+                                    {searchByOption}
+                                </li>
+                    })}
+                </ul>
+            </div>
+            <div className="SearchBar">
+                <input placeholder="Enter A Song, Album, or Artist" onChange={handleTermChange}/>
+                <button className="SearchButton" onClick={search}>SEARCH</button>
+            </div>
         </div>
+
     );
 }
 
