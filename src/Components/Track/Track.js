2ea5fb6ec753df624ac311af4252af9c63eff499
mkamilgok <mkamilgok@gmail.com>
2020-08-10 11:42:20 +0300

    Fix bugs in Track.js due to curly braces

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -5,8 +5,8 @@ function Track(props){
     return (
         <div className="Track">
             <div className="Track-information">
-                <h3>props.track.name</h3>
-                <p>props.track.artist | props.track.album</p>
+                <h3>{props.track.name}</h3>
+                <p>{props.track.artist} | {props.track.album}</p>
             </div>
             <button className="Track-action"> + or - will go here --></button>
         </div>
